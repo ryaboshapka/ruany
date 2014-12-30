bae0cd8ade1998ab2b0c819a057ca9f88c75092c
ryaboshapka <remagnetization@gmail.com>
2014-12-30 23:21:20 +0300

    cv

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
+// Example showing how to read and write images
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv/cvaux.hpp>
 
-using namespace std;
-
-int main()
+int main(int argc, char** argv)
 {
-    cout << "Dolloru kaput!" << endl;
+    IplImage * pInpImg = 0;
+
+    // Load an image from file - change this based on your image name
+    pInpImg = cvLoadImage("/Users/abricos/Desktop/kedyaev.jpg", CV_LOAD_IMAGE_UNCHANGED);
+    if(!pInpImg)
+    {
+        fprintf(stderr, "failed to load input image\n");
+        return -1;
+    }
+
+    // Write the image to a file with a different name,
+    // using a different image format -- .png instead of .jpg
+    if( !cvSaveImage("/Users/abricos/Desktop/kedyaev2.png", pInpImg) )
+    {
+        fprintf(stderr, "failed to write image file\n");
+    }
+
+    // Remember to free image memory after using it!
+    cvReleaseImage(&pInpImg);
+
     return 0;
 }
-
--- ruany.pro
+++ ruany.pro
@@ -8,3 +8,13 @@ SOURCES += main.cpp
 include(deployment.pri)
 qtcAddDeployment()
 
+
+win32:CONFIG(release, debug|release): LIBS += -L/usr/local/lib/release/ -lopencv_video -lopencv_superres -lopencv_ocl -lopencv_stitching -lopencv_photo
+else:win32:CONFIG(debug, debug|release): LIBS += -L/usr/local/lib/debug/ -lopencv_video -lopencv_superres -lopencv_ocl -lopencv_stitching -lopencv_photo
+else:unix: LIBS += -L/usr/local/lib/ -lopencv_video -lopencv_superres -lopencv_ocl -lopencv_stitching -lopencv_photo
+INCLUDEPATH += /usr/local/include:/usr/local/include/opencv
+DEPENDPATH += /usr/local/include:/usr/local/include/opencv
+
+
+INCLUDEPATH += /usr/local/include/opencv2
+DEPENDPATH += /usr/local/include/opencv2
